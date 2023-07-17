f935b5a01834ab68b48e9f3294b53dc7541cf072
Grishchenko Nadezhda <130903440+GrishchenkoNadezhda@users.noreply.github.com>
2023-07-17 23:55:21 +0700

    Add files via upload

--- src/js/script.js
+++ src/js/script.js
@@ -1,21 +1,24 @@
-/*========== menu icon navbar ==========*/
-let menuIcon = document.querySelector('#menu-icon');
+/*========== значок меню на навигационной панели ==========*/
+/* let menuIcon = document.querySelector('#menu-icon');
 let navbar = document.querySelector('.navbar');
+ */
 
-
-/*========== scroll sections active link ==========*/
-let sections = document.querySelectorAll('section');
+/*========== активная ссылка на разделы прокрутки ==========*/
+/* let sections = document.querySelectorAll('section');
 let navLinks = document.querySelectorAll('header nav a');
 
-window.onscroll = () => {
+window.onscroll = () => { 
 
 
-    /*========== sticky navbar ==========*/
-
-
-    /*========== remove menu icon navbar when click navbar link (scroll) ==========*/
-
+/*========== липкая панель навигации ==========*/
+window.onscroll = () => { 
+let header = document.querySelector('header');
+header.classList.toggle('sticky', window.scrollY > 100);
 };
+
+/*========== remove menu icon navbar when click navbar link (scroll) ==========*/
+
+ 
 
 
 /*========== swiper ==========*/
