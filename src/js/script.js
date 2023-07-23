e364dd97a586433a36c10ac87ec51ea2f9d72f1e
Grishchenko Nadezhda <130903440+GrishchenkoNadezhda@users.noreply.github.com>
2023-07-23 22:26:28 +0700

    Add files via upload

--- src/js/script.js
+++ src/js/script.js
@@ -4,14 +4,27 @@ let navbar = document.querySelector('.navbar');
  */
 
 /*========== активная ссылка на разделы прокрутки ==========*/
-/* let sections = document.querySelectorAll('section');
+let sections = document.querySelectorAll('section');
 let navLinks = document.querySelectorAll('header nav a');
 
 window.onscroll = () => { 
+  sections.forEach(sec => {
+    let top = window.scrollY;
+    let offset = sec.offsetTop - 150;
+    let height = sec.offsetHeight;
+    let id = sec.getAttribute('id');
+    if(top >= offset && top < offset + height) { 
+      navLinks.forEach(links => {
+        links.classList.remove('active');
+        document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+      });
+    };
+
+  });
 
 
 /*========== липкая панель навигации ==========*/
-window.onscroll = () => { 
+
 let header = document.querySelector('header');
 header.classList.toggle('sticky', window.scrollY > 100);
 };
@@ -22,7 +35,20 @@ header.classList.toggle('sticky', window.scrollY > 100);
 
 
 /*========== swiper ==========*/
-
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 1,
+  spaceBetween: 50,
+  grabCursor: true,
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
 
 /*========== dark light mode ==========*/
  let darkModeIcon = document.querySelector('#darkMode-icon');
